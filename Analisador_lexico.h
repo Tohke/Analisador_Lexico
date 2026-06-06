98a4a702b0f33b7a8493d18ac3f95a0f05a15331
jsouzaTohke <tohkekun286@gmail.com>
2026-06-06 19:29:45 -0300

    mudando para biblioteca

---
+++ Analisador_lexico.h
@@ -0,0 +1,250 @@
+#pragma once
+
+#include <iostream>
+#include <string>
+#include <unordered_map>
+#include <stdexcept>
+#include <cctype>
+#include <iomanip>
+#include <vector>
+
+using namespace std;
+
+// Enumeração que representa todos os tipos de tokens
+enum class TokenType {
+    T_IF, T_ELSE, T_WHILE, T_PRINTLN,
+    T_FN, T_LET, T_MUT, T_MATCH, T_UL, T_BANG, T_REF, T_COLON,
+    T_ARROW, T_FAT_ARROW, T_STRING, T_COMMA,
+    T_ID, T_NUM, T_TYPE,
+    T_ASSIGN, T_EQ,
+    T_PLUS, T_MINUS, T_MULT, T_DIV,
+    T_LT, T_GT,
+    T_LPAREN, T_RPAREN, T_LBRACE, T_RBRACE, T_SEMICOLON,
+    T_EOF
+};
+
+// Estrutura que representa um token
+struct Token {
+    TokenType type;
+    string lexeme;
+    int line;
+
+    Token(TokenType t, string l, int ln) : type(t), lexeme(l), line(ln) {}
+};
+
+// Classe do Scanner
+class Scanner {
+private:
+    string input;
+    size_t pos;
+    int line;
+    unordered_map<string, TokenType> keywords;
+
+public:
+    Scanner(string source) : input(source), pos(0), line(1) {
+        keywords["if"] = TokenType::T_IF;
+        keywords["else"] = TokenType::T_ELSE;
+        keywords["while"] = TokenType::T_WHILE;
+        keywords["println"] = TokenType::T_PRINTLN;
+        keywords["let"] = TokenType::T_LET;
+        keywords["mut"] = TokenType::T_MUT;
+        keywords["fn"] = TokenType::T_FN;
+        keywords["match"] = TokenType::T_MATCH;
+        keywords["_"] = TokenType::T_UL;
+        keywords["i32"] = TokenType::T_TYPE;
+        keywords["u32"] = TokenType::T_TYPE;
+        keywords["f32"] = TokenType::T_TYPE;
+        keywords["bool"] = TokenType::T_TYPE;
+        keywords["char"] = TokenType::T_TYPE;
+        keywords["str"]  = TokenType::T_TYPE;
+    }
+
+    char peek() {
+        if (pos >= input.length()) return '\0';
+        return input[pos];
+    }
+
+    char next() {
+        char c = peek();
+        if (c != '\0') pos++;
+        return c;
+    }
+
+    void skipWhitespace() {
+        while (isspace(peek())) {
+            if (next() == '\n') line++;
+        }
+    }
+
+    void skipComment() {
+        while (peek() != '\n' && peek() != '\0') next();
+    }
+
+    void skipMultilineComment() {
+        while (true) {
+            if (peek() == '\0') {
+                throw runtime_error("Erro Lexico: comentario multilinha nao fechado na linha " + to_string(line));
+            }
+            if (peek() == '\n') line++;
+            if (peek() == '*') {
+                next();
+                if (peek() == '/') {
+                    next();
+                    break;
+                }
+                continue;
+            }
+            next();
+        }
+    }
+
+    Token scanNumber(char start) {
+        string buffer;
+        buffer += start;
+        char c = start;
+        bool isFloat = false;
+
+        while (isdigit(c) || c == '.') {
+            if(c == '.'){
+                if(isFloat){
+                    throw runtime_error("Erro Lexico: Float com dois pontos na Linha " + to_string(line));
+                }
+                isFloat = true;
+            }
+            buffer += next();
+            c = peek();
+        }
+        return Token(TokenType::T_NUM, buffer, line);
+    }
+
+    Token scanString(){
+        string buffer = "\"";
+        while (peek() != '"' && peek() != '\0') {
+            if (peek() == '}'){
+                throw runtime_error("Erro Lexico: placeholder invalido na linha " + to_string(line));
+            }
+            if (peek() == '{') {
+                buffer += next();
+                if (peek() == '}') {
+                    buffer += next();
+                    continue;
+                }
+                throw runtime_error("Erro Lexico: placeholder invalido na linha " + to_string(line));
+            }
+            buffer += next();
+        }
+        if (peek() == '"') {
+            buffer += next();
+        } else {
+            throw runtime_error("Erro Lexico: String nao fechada na linha " + to_string(line));
+        }
+        return Token(TokenType::T_STRING, buffer, line);
+    }
+
+    Token scanIdentifier(char start) {
+        string buffer;
+        buffer += start;
+        while (isalnum(peek()) || peek() == '_') {
+            buffer += next();
+        }
+        if (keywords.count(buffer)) {
+            return Token(keywords[buffer], buffer, line);
+        }
+        return Token(TokenType::T_ID, buffer, line);
+    }
+
+    Token nextToken() {
+        skipWhitespace();
+        if (pos >= input.length()) return Token(TokenType::T_EOF, "", line);
+
+        char c = next();
+        if (isdigit(c)) return scanNumber(c);
+        if (isalpha(c) || c == '_') return scanIdentifier(c);
+        if(c == '"') return scanString();
+
+        switch (c) {
+        case '+': return Token(TokenType::T_PLUS, "+", line);
+        case '-':
+            if (peek() == '>'){
+                next();
+                return Token(TokenType::T_ARROW, "->", line);
+            }
+            return Token(TokenType::T_MINUS, "-", line);
+        case '*': return Token(TokenType::T_MULT, "*", line);
+        case '/':
+            if (peek() == '/') {
+                next();
+                skipComment();
+                return nextToken();
+            }
+            if (peek() == '*') {
+                next();
+                skipMultilineComment();
+                return nextToken();
+            }
+            return Token(TokenType::T_DIV, "/", line);
+        case '=':
+            if (peek() == '=') {
+                next();
+                return Token(TokenType::T_EQ, "==", line);
+            }
+            if (peek() == '>'){
+                next();
+                return Token(TokenType::T_FAT_ARROW, "=>", line);
+            }
+            return Token(TokenType::T_ASSIGN, "=", line);
+        case '!': return Token(TokenType::T_BANG, "!", line);
+        case '&': return Token(TokenType::T_REF, "&", line);
+        case ':': return Token(TokenType::T_COLON, ":", line); // Corrigido de T_REF para T_COLON
+        case ',': return Token(TokenType::T_COMMA, ",", line);
+        case '<': return Token(TokenType::T_LT, "<", line);
+        case '>': return Token(TokenType::T_GT, ">", line);
+        case '(': return Token(TokenType::T_LPAREN, "(", line);
+        case ')': return Token(TokenType::T_RPAREN, ")", line);
+        case '{': return Token(TokenType::T_LBRACE, "{", line);
+        case '}': return Token(TokenType::T_RBRACE, "}", line);
+        case ';': return Token(TokenType::T_SEMICOLON, ";", line);
+        default:
+            throw runtime_error("Erro Lexico: caractere invalido '" + string(1, c) + "' na linha " + to_string(line));
+        }
+    }
+};
+
+inline string tokenTypeToString(TokenType type) {
+    switch(type) {
+        case TokenType::T_IF: return "T_IF";
+        case TokenType::T_ELSE: return "T_ELSE";
+        case TokenType::T_WHILE: return "T_WHILE";
+        case TokenType::T_PRINTLN: return "T_PRINTLN";
+        case TokenType::T_ID: return "T_ID";
+        case TokenType::T_NUM: return "T_NUM";
+        case TokenType::T_FN: return "T_FN";
+        case TokenType::T_LET: return "T_LET";
+        case TokenType::T_MUT: return "T_MUT";
+        case TokenType::T_MATCH: return "T_MATCH";
+        case TokenType::T_UL: return "T_UL";
+        case TokenType::T_BANG: return "T_BANG";
+        case TokenType::T_REF: return "T_REF";
+        case TokenType::T_COLON: return "T_COLON";
+        case TokenType::T_ARROW: return "T_ARROW";
+        case TokenType::T_FAT_ARROW: return "T_FAT_ARROW";
+        case TokenType::T_TYPE: return "T_TYPE";
+        case TokenType::T_STRING: return "T_STRING";
+        case TokenType::T_COMMA: return "T_COMMA";
+        case TokenType::T_ASSIGN: return "T_ASSIGN";
+        case TokenType::T_EQ: return "T_EQ";
+        case TokenType::T_PLUS: return "T_PLUS";
+        case TokenType::T_MINUS: return "T_MINUS";
+        case TokenType::T_MULT: return "T_MULT";
+        case TokenType::T_DIV: return "T_DIV";
+        case TokenType::T_LT: return "T_LT";
+        case TokenType::T_GT: return "T_GT";
+        case TokenType::T_LPAREN: return "T_LPAREN";
+        case TokenType::T_RPAREN: return "T_RPAREN";
+        case TokenType::T_LBRACE: return "T_LBRACE";
+        case TokenType::T_RBRACE: return "T_RBRACE";
+        case TokenType::T_SEMICOLON: return "T_SEMICOLON";
+        case TokenType::T_EOF: return "T_EOF";
+        default: return "UNKNOWN";
+    }
+}
--- Parser.cpp
+++ Parser.cpp
@@ -1,48 +1,29 @@
 #include <iostream>
 #include <vector>
+#include <string>
+#include <stdexcept>
+// Importa o Analisador Léxico (certifique-se de que o Analisador_lexico.h está na mesma pasta)
+#include "Analisador_lexico.h"
 
-/*
-TODO: Falta implementar:
-    - FEITO parseAssignment(): Validação de atribuição de variáveis já declaradas (ex: soma = 30;).
-    - parsePrintStmt(): Adaptação da regra de impressão para o modelo do Rust (ex: validar a sintaxe exata de println!("{}", soma);).
-    - parseIf(): Estrutura condicional. Diferente do MicroC original, em Rust não precisamos exigir parênteses ao redor da expressão, mas as chaves {} são estritamente obrigatórias.
-    - parseWhile(): Laço de repetição, seguindo a mesma lógica de parênteses opcionais e chaves obrigatórias do if.
-    - AST
-    - Aprimoramento do Tratamento de Erros
-    - Melhorias
- */
+using namespace std;
 
-
-
-
-
-//Parser Descendente Recursivo -> cada regra da gramática se torna uma funcão no código
 class Parser {
 private:
-    vector<Token> tokens; // Lista de tokens gerada pelo Scanner
-    size_t pos;           // Controle de posição dos tokens
+    vector<Token> tokens;
+    size_t pos;
 public:
-    // Construtor recebe a lista de tokens do Scanner
     Parser(vector<Token> t) : tokens(t), pos(0) {}
 
-    // ============================================== // ==============================================
-    // Olha o token atual sem avançar
     Token peek() {
-        if (pos >= tokens.size()) {
-            return tokens.back(); // Retorna o EOF de segurança
-        }
+        if (pos >= tokens.size()) return tokens.back();
         return tokens[pos];
     }
-    // ============================================== // ==============================================
-    // Retorna o token atual e avança para o próximo
+
     Token advance() {
-        if (pos < tokens.size()) {
-            pos++;
-        }
+        if (pos < tokens.size()) pos++;
         return tokens[pos - 1];
     }
-    // ============================================== // ==============================================
-    // Verifica se o token atual é o esperado. Se for, consome e retorna true.
+
     bool match(TokenType expected) {
         if (peek().type == expected) {
             advance();
@@ -50,8 +31,7 @@ public:
         }
         return false;
     }
-    // ============================================== // ==============================================
-    // Tratamento de erros utilizando Panic Mode
+
     void error(string message) {
         Token t = peek();
         throw runtime_error(
@@ -60,103 +40,83 @@ public:
         );
     }
 
-    // ============================================== // ==============================================
     void parseProgram() {
-        // Regra: Program -> Statement*
         while (peek().type != TokenType::T_EOF) {
             parseStatement();
         }
     }
-    // ============================================== // ==============================================
-    void parseExpression() {
-            // Toda expressão começa com pelo menos um Termo
+
+    void parseAdditive() {
+        parseTerm();
+        while (peek().type == TokenType::T_PLUS || peek().type == TokenType::T_MINUS) {
+            advance();
             parseTerm();
-
-            // Enquanto encontrar + ou -, continua consumindo novos termos
-            while (peek().type == TokenType::T_PLUS || peek().type == TokenType::T_MINUS) {
-                advance();   // Consome o operador (+ ou -)
-                parseTerm(); // Avalia o próximo lado da operação
-            }
         }
-    // ============================================== // ==============================================
+    }
+
+    void parseExpression() {
+        parseAdditive();
+        TokenType type = peek().type;
+        if (type == TokenType::T_EQ || type == TokenType::T_LT || type == TokenType::T_GT) {
+            advance();
+            parseAdditive();
+        }
+    }
+
     void parseTerm() {
-            // Todo termo começa com pelo menos um Fator
+        parseFactor();
+        while (peek().type == TokenType::T_MULT || peek().type == TokenType::T_DIV) {
+            advance();
             parseFactor();
-
-            // Enquanto encontrar * ou /, continua consumindo novos fatores
-            while (peek().type == TokenType::T_MULT || peek().type == TokenType::T_DIV) {
-                advance();     // Consome o operador (* ou /)
-                parseFactor(); // Avalia o próximo lado da operação
-            }
         }
-    // ============================================== // ==============================================
+    }
+
     void parseFactor() {
-            TokenType type = peek().type;
-
-            // Possibilidades:
-            // Ser um número
-            if (type == TokenType::T_NUM) {
-                advance(); // Consome o número
-            }
-            // Ser uma variável (identificador)
-            else if (type == TokenType::T_ID) {
-                advance(); // Consome a variável
-            }
-            // Ser uma nova expressão entre parênteses
-            else if (type == TokenType::T_LPAREN) {
-                advance(); // Consome o '('
-
-                parseExpression(); // Volta recursivamente para avaliar o que está dentro
-
-                if (!match(TokenType::T_RPAREN)) {
-                    error("Esperado ')' apos expressao");
-                }
-            }
-            else {
-                error("Esperado numero, variavel ou '(' na expressao");
-            }
+        TokenType type = peek().type;
+        if (type == TokenType::T_NUM) {
+            advance();
         }
-    // ============================================== // ==============================================
-    void parseDeclaration() {
-            match(TokenType::T_LET); // Consome o 'let'
-
-            // Suporte a variáveis mutáveis (ex: let mut x = 10;) -> mut
-            if (peek().type == TokenType::T_MUT) {
-                advance(); // Consome o 'mut'
-            }
-
-            // Verifica se é um identificador válido
-            if (!match(TokenType::T_ID)) {
-                error("Esperado nome da variavel apos 'let'");
-            }
-
-            // Verifica se tem tipagem explícita (ex: let num1: i32 = 10;) -> i32, i64, ...
-            if (peek().type == TokenType::T_COLON) {
-                advance(); // Consome o ':'
-                if (!match(TokenType::T_TYPE)) {
-                    error("Esperado tipo da variavel (ex: i32) apos ':'");
-                }
-            }
-// TODO: Talvez precise mudar algo para igualdade ==
-            // Deve ser seguido por '='
-            if (!match(TokenType::T_ASSIGN)) {
-                error("Esperado '=' na declaracao");
-            }
-            // Chama o parse de expressões matemáticas (que vamos criar depois)
+        else if (type == TokenType::T_ID) {
+            advance();
+        }
+        else if (type == TokenType::T_LPAREN) {
+            advance();
             parseExpression();
-
-            // Toda declaração no final exige ponto e vírgula
-            if (!match(TokenType::T_SEMICOLON)) {
-                error("Esperado ';' no final da instrucao");
+            if (!match(TokenType::T_RPAREN)) {
+                error("Esperado ')' apos expressao");
             }
         }
-    // ============================================== // ==============================================
-    // Funcão na qual decide qual regra gramatical vai utilizar ao olhar para o primeiro token da instrucão atual
+        else {
+            error("Esperado numero, variavel ou '(' na expressao");
+        }
+    }
+
+    void parseDeclaration() {
+        match(TokenType::T_LET);
+        if (peek().type == TokenType::T_MUT) {
+            advance();
+        }
+        if (!match(TokenType::T_ID)) {
+            error("Esperado nome da variavel apos 'let'");
+        }
+        if (peek().type == TokenType::T_COLON) {
+            advance();
+            if (!match(TokenType::T_TYPE)) {
+                error("Esperado tipo da variavel (ex: i32) apos ':'");
+            }
+        }
+        if (!match(TokenType::T_ASSIGN)) {
+            error("Esperado '=' na declaracao");
+        }
+        parseExpression();
+        if (!match(TokenType::T_SEMICOLON)) {
+            error("Esperado ';' no final da instrucao");
+        }
+    }
+
     void parseStatement() {
         TokenType type = peek().type;
-
-        // Verifica qual o token atual para decidir a regra de derivação
-        if (type == TokenType::T_LET) { // Aqui por exemplo, seria INT em C
+        if (type == TokenType::T_LET) {
             parseDeclaration();
         }
         else if (type == TokenType::T_PRINTLN) {
@@ -175,124 +135,122 @@ public:
             error("Comando invalido ou inesperado");
         }
     }
-    // ============================================== // ==============================================
+
     void parseAssignment() {
-        // Consome o nome da variável (identificador)
         if (!match(TokenType::T_ID)) {
             error("Esperado nome da variavel para atribuicao");
         }
-
-        // Verifica se há o operador de atribuição '='
         if (!match(TokenType::T_ASSIGN)) {
             error("Esperado '=' apos o identificador");
         }
-
-        // Chama a análise da expressão para o lado direito da igualdade
         parseExpression();
-
-        // Toda atribuição deve terminar com ponto e vírgula
         if (!match(TokenType::T_SEMICOLON)) {
             error("Esperado ';' no final da instrucao de atribuicao");
         }
     }
-    // ============================================== // ==============================================
+
     void parseIf() {
-        match(TokenType::T_IF); // Consome 'if'
-
-        // Avalia a expressão (condição). Em Rust, parênteses não são obrigatórios aqui.
+        match(TokenType::T_IF);
         parseExpression();
-
-        // O bloco do if deve obrigatoriamente começar com '{' em Rust
         if (!match(TokenType::T_LBRACE)) {
             error("Esperado '{' antes do bloco do if");
         }
-
-        // Continua fazendo o parse de instruções até encontrar o fechamento '}'
         while (peek().type != TokenType::T_RBRACE && peek().type != TokenType::T_EOF) {
             parseStatement();
         }
-
         if (!match(TokenType::T_RBRACE)) {
             error("Esperado '}' apos o bloco do if");
         }
-
-        // Verifica se há um bloco 'else' opcional
         if (peek().type == TokenType::T_ELSE) {
-            advance(); // Consome 'else'
-
+            advance();
             if (!match(TokenType::T_LBRACE)) {
                 error("Esperado '{' antes do bloco do else");
             }
-
             while (peek().type != TokenType::T_RBRACE && peek().type != TokenType::T_EOF) {
                 parseStatement();
             }
-
             if (!match(TokenType::T_RBRACE)) {
                 error("Esperado '}' apos o bloco do else");
             }
         }
     }
-    // ============================================== // ==============================================
+
     void parseWhile() {
-            match(TokenType::T_WHILE); // Consome 'while'
-
-            // Avalia a expressão que atua como condição de parada
-            parseExpression();
-
-            // Escopo do laço deve começar com '{'
-            if (!match(TokenType::T_LBRACE)) {
-                error("Esperado '{' antes do bloco do while");
-            }
-
-            // Faz o parse das instruções internas do laço
-            while (peek().type != TokenType::T_RBRACE && peek().type != TokenType::T_EOF) {
-                parseStatement();
-            }
-
-            if (!match(TokenType::T_RBRACE)) {
-                error("Esperado '}' apos o bloco do while");
-            }
+        match(TokenType::T_WHILE);
+        parseExpression();
+        if (!match(TokenType::T_LBRACE)) {
+            error("Esperado '{' antes do bloco do while");
         }
-// ============================================== // ==============================================
-void parsePrintStmt() {
-        match(TokenType::T_PRINTLN); // Consome 'println'
+        while (peek().type != TokenType::T_RBRACE && peek().type != TokenType::T_EOF) {
+            parseStatement();
+        }
+        if (!match(TokenType::T_RBRACE)) {
+            error("Esperado '}' apos o bloco do while");
+        }
+    }
 
-        // Em Rust, println é uma macro, o que exige a exclamação
+    void parsePrintStmt() {
+        match(TokenType::T_PRINTLN);
         if (!match(TokenType::T_BANG)) {
             error("Esperado '!' apos println");
         }
-
         if (!match(TokenType::T_LPAREN)) {
             error("Esperado '(' apos println!");
         }
-
-        // Exige uma string de formatação literal (ex: "{}")
         if (!match(TokenType::T_STRING)) {
             error("Esperado string de formatacao (ex: \"{}\")");
         }
-
-        // Se houver uma vírgula, significa que há variáveis ou expressões a serem inseridas
         if (peek().type == TokenType::T_COMMA) {
-            advance(); // Consome ','
-
-            // Avalia o que será impresso (pode ser uma variável ou uma conta matemática inteira)
+            advance();
             parseExpression();
         }
-
         if (!match(TokenType::T_RPAREN)) {
             error("Esperado ')' fechando o println!");
         }
-
         if (!match(TokenType::T_SEMICOLON)) {
             error("Esperado ';' no final da instrucao println!");
         }
     }
-// ============================================== // ==============================================
-
-
-    // Funções de Expressão Matemática
-    // void parseExpression() {}
-    // void parseTerm() {}
-    // void parseFactor() {}
 };
+
+int main() {
+    string code = R"(
+    let num1: i32 = 10;     // Declaração usando identificador
+    let num2 = 20;          // Declaração implicida
+
+    let soma = num1 + num2;
+
+    if soma == 30 {
+        println!("{}", soma);
+    }
+    /*
+    let numf = 2.5;         // Float correto
+    let numf2 = 2.5.5       // Gera erro
+    */
+    )";
+
+    Scanner scanner(code);
+
+    try {
+        vector<Token> tokens;
+        Token token = scanner.nextToken();
+
+        while (token.type != TokenType::T_EOF) {
+            tokens.push_back(token);
+            token = scanner.nextToken();
+        }
+        tokens.push_back(token); // Adiciona o EOF
+
+        Parser parser(tokens);
+        cout << "Iniciando a analise sintatica (Parsing)..." << endl;
+
+        parser.parseProgram();
+
+        cout << "Analise sintatica concluida com sucesso! Nenhum erro encontrado." << endl;
+    }
+    catch (exception& e) {
+        cerr << e.what() << endl;
+    }
+
+    return 0;
+}
